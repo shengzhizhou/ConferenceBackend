0827f9a48741dccf39044d584e37c33016e4f81f
shengzhizhou <40223156+shengzhizhou@users.noreply.github.com>
2019-05-29 00:25:57 -0400

    final_draft_unfinished

--- cewit/src/main/java/cewit/map/model/AlgoType.java
+++
@@ -1,9 +0,0 @@
-package cewit.map.model;
-
-import java.io.Serializable;
-
-public enum AlgoType implements Serializable {
-    GraphParttiiton,
-    SimuAnneal
-}
-
--- cewit/src/main/java/cewit/map/model/Party.java
+++
@@ -1,8 +0,0 @@
-//package cse308.map.model;
-//
-//import java.io.Serializable;
-//
-//public enum Party  implements Serializable {
-//    DEMOCRATIC,
-//    REPUBLICAN
-//}
